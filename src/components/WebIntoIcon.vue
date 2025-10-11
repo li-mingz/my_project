12c1fd4c47d7727638ae5ebca13f3e1eb56ef3e9
17330915383 <lm17330915383@163.com>
2025-10-11 20:10:59 +0800

    基本位移

--- src/components/WebIntoIcon.vue
+++ src/components/WebIntoIcon.vue
@@ -1,23 +1,30 @@
 
 <script setup>
 import Lottie from 'lottie-web';
-import { onMounted } from 'vue';
-
+import { onMounted, ref } from 'vue';
+// 动画阶段
+const stage = ref(0);
 // 确保DOM挂载后再加载动画
 onMounted(() => {
   Lottie.loadAnimation({
     container: document.getElementById("container_box"),
     renderer: 'svg',
-    loop: true,
+    loop: false,
     autoplay: true,
     path: './src/assets/lottie/WebIntoIcon.json'
   });
+  // 开始动画序列
+  stage.value = 1; // 第一阶段
+  setTimeout(() => {
+    stage.value = 2; // 第二阶段
+  }, 3000);
 });
 </script>
 
 <template>
   <div id="container">
-    <div id="container_box">
+    <div id="container_box"
+      :class="{ 'container_box_stage_2': stage >= 2 }">
     </div>
   </div>
 </template>
@@ -32,9 +39,23 @@ onMounted(() => {
     height: 100vh;
   }
   #container_box {
-    position: relative;
-    width: 20rem;
-    height: 20rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    /* 居中偏移 */
+    transform: translate(-50%, -50%);
+    width: 30rem;
+    height: 30rem;
+    transition: all 1s ease-in-out;
+  }
+  #container_box.container_box_stage_2 {
+    /* 定位到左上角 */
+    top: 0;
+    left: 0;
+    transform: translate(0, 0);
+    background-color: bisque;
+    width: 10rem;
+    height: 10rem;
   }
 
 </style>
