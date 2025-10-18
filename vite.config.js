03e34cf04f85a02850292776a89162c01b7ea1b9
17330915383 <lm17330915383@163.com>
2025-10-18 18:10:33 +0800

    优化js打包

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +12,27 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    visualizer({
+      filename: 'stats/stats.html' // 分析报告保存路径
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // 自定义 chunk 分割规则
+        manualChunks: {
+          // 把大型库单独打包
+          lottie: ['lottie-web'],
+          katex: ['katex'],
+          elementPlus: ['element-plus'],
+          highlightJs: ['highlight.js']
+        }
+      }
+    }
   },
 })
